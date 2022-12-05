fdd97f2112497d3052f88b80c8f9d8141bb6ad32
Nicolas Picard <olacin@users.noreply.github.com>
2022-12-05 10:46:57 +0100

    test(2022): increase day 5 coverage

--- 2022/tests/test_day05.py
+++ 2022/tests/test_day05.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
-from advent_of_code.day05 import part1, part2
+import pytest
+
+from advent_of_code.day05 import parse_op, part1, part2
 
 INPUT = Path(__file__).parent / "data" / "day05.txt"
 
@@ -11,3 +13,8 @@ def test_part1():
 
 def test_part2():
     assert part2(INPUT) == "TPFFBDRJD"
+
+
+def test_failing_parse_op():
+    with pytest.raises(ValueError):
+        parse_op("another one bites the dust")
