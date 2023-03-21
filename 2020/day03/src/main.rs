a75c957c58eb50514c9bd95327c0d076fcb45e45
olacin <olacin@users.noreply.github.com>
2023-03-22 00:10:59 +0100

    feat(2020): add day 3 part 2

--- 2020/day03/src/main.rs
+++ 2020/day03/src/main.rs
@@ -1,24 +1,41 @@
-fn problem(right: usize) -> usize {
-    // TODO: make this work with down parameter
-    let content: Vec<&[u8]> = include_str!("data.txt")
+fn read_data() -> Vec<&'static [u8]> {
+    include_str!("data.txt")
         .lines()
         .map(|line| line.as_bytes())
-        .collect();
-
-    let map_width = content[0].len();
-    let trees = (0..content.len() - 1)
-        .map(|idx| ((right * (idx + 1)) % map_width, idx + 1))
-        .filter(|(x, y)| content[*y][*x] == b'#')
-        .count();
-    trees
+        .collect()
 }
 
-fn part2() -> usize {
-    let slopes = vec![1, 3, 5, 7, 1];
-    slopes.iter().map(|slope| problem(*slope)).product()
+fn problem(content: &Vec<&[u8]>, right: usize, down: usize) -> usize {
+    let map_width = content[0].len();
+    content
+        .iter()
+        .step_by(down)
+        .enumerate()
+        .filter(|(idx, row)| row[idx * right % map_width] == b'#')
+        .count()
+}
+
+fn part2(data: &Vec<&[u8]>) -> usize {
+    vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|(right, down)| problem(data, *right, *down))
+        .product()
 }
 
 fn main() {
-    println!("{}", problem(3));
-    println!("{}", part2());
+    let data = read_data();
+    println!("Part 1: {}", problem(&data, 3, 1));
+    println!("Part 2: {}", part2(&data));
+}
+
+#[test]
+fn test_part1() {
+    let data = read_data();
+    assert_eq!(problem(&data, 3, 1), 284);
+}
+
+#[test]
+fn test_part2() {
+    let data = read_data();
+    assert_eq!(part2(&data), 3510149120);
 }
